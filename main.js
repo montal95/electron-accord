0293158632e5bb3567fe6d78288ac075126daf88
Sam M <sammontalvojr@gmail.com>
2021-04-09 17:11:45 -0500

    added code to initialize the first window

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(createWindow);
