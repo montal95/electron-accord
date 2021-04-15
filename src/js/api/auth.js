2ff9e24e7a3b35e20dacc38662bb50b06d62b1af
Sam M <sammontalvojr@gmail.com>
2021-04-14 19:18:07 -0500

    register user to firebase and created profile collection with registration

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,11 +1,25 @@
+import db from "../db/firestore";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-export const register = async ({ email, password }) => {
+const createUserProfile = (userProfile) =>
+  db.collection("profiles").doc(userProfile.uid).set(userProfile);
+
+export const register = async ({ email, password, username, avatar }) => {
   try {
     const { user } = await firebase
       .auth()
       .createUserWithEmailAndPassword(email, password);
+      
+    await createUserProfile({
+      uid: user.uid,
+      username,
+      password,
+      avatar,
+      email,
+      joinedChats: [],
+    });
+
     return user;
   } catch (error) {
     return Promise.reject(error.message);
