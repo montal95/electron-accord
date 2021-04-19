0e0c0be2deb43bfe757d175a47f87c4f6c4ee4a0
Sam M <sammontalvojr@gmail.com>
2021-04-19 15:15:03 -0500

    creating a reusable reducer for errorhandling. Common.js created for reusable reducers

--- src/js/reducers/auth.js
+++ src/js/reducers/auth.js
@@ -1,38 +1,15 @@
 import { combineReducers } from "redux";
+import { createErrorReducer } from "./common";
 
-const createLoginReducer = () => {
-  const error = (state = null, action) => {
-    switch (action.type) {
-      case "AUTH_LOGIN_INIT":
-        return null;
-      case "AUTH_LOGIN_ERROR":
-        return action.error;
-      default:
-        return state;
-    }
-  };
-
-  return combineReducers({
-    error,
+const createLoginReducer = () =>
+  combineReducers({
+    error: createErrorReducer("AUTH_LOGIN"),
   });
-};
 
-const createRegisterReducer = () => {
-  const error = (state = null, action) => {
-    switch (action.type) {
-      case "AUTH_REGISTER_INIT":
-        return null;
-      case "AUTH_REGISTER_ERROR":
-        return action.error;
-      default:
-        return state;
-    }
-  };
-
-  return combineReducers({
-    error,
+const createRegisterReducer = () =>
+  combineReducers({
+    error: createErrorReducer("AUTH_REGISTER"),
   });
-};
 
 const createAuthReducer = () => {
   const user = (state = null, action) => {
