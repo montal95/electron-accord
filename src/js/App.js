e9cf74098fe62a362fcd65b52f0484784d60809d
Sam M <sammontalvojr@gmail.com>
2021-04-21 13:57:35 -0500

    created the authroute wrapper to handle redirecting the user after logging out in the appjs

--- src/js/App.js
+++ src/js/App.js
@@ -2,7 +2,12 @@ import React, { useEffect } from "react";
 import { Chat, Home, Welcome, Settings } from "./views";
 import { Navbar } from "./components";
 
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
 import { listenToAuthChanges } from "./actions/auth";
@@ -12,6 +17,24 @@ import { LoadingView } from "./components/shared/LoadingView";
 const ContentWrapper = ({ children }) => (
   <div className="content-wrapper">{children}</div>
 );
+
+const AuthRoute = ({ children, ...rest }) => {
+  const user = useSelector(({ auth }) => auth.user);
+  const onlyChild = React.Children.only(children);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        user ? (
+          React.cloneElement(onlyChild, { ...rest, ...props })
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    />
+  );
+};
 
 const ChatApp = () => {
   const dispatch = useDispatch();
@@ -31,15 +54,15 @@ const ChatApp = () => {
           <Route exact path="/">
             <Welcome />
           </Route>
-          <Route path="/home">
+          <AuthRoute path="/home">
             <Home />
-          </Route>
-          <Route path="/chat/:id">
+          </AuthRoute>
+          <AuthRoute path="/chat/:id">
             <Chat />
-          </Route>
-          <Route path="/settings">
+          </AuthRoute>
+          <AuthRoute path="/settings">
             <Settings />
-          </Route>
+          </AuthRoute>
         </Switch>
       </ContentWrapper>
     </Router>
