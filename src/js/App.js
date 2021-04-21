b097ffdb7648b90a176e93038e67f84d6a984459
Sam M <sammontalvojr@gmail.com>
2021-04-21 13:45:14 -0500

    refactoring appjs, created store provider wrapper, and moved ischecking into appjs and out of login and register forms

--- src/js/App.js
+++ src/js/App.js
@@ -3,40 +3,54 @@ import { Chat, Home, Welcome, Settings } from "./views";
 import { Navbar } from "./components";
 
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-import configureStore from "./store/index";
 import { listenToAuthChanges } from "./actions/auth";
+import StoreProvider from "./store/StoreProvider";
+import { LoadingView } from "./components/shared/LoadingView";
 
-const store = configureStore();
+const ContentWrapper = ({ children }) => (
+  <div className="content-wrapper">{children}</div>
+);
 
-const App = () => {
+const ChatApp = () => {
+  const dispatch = useDispatch();
+  const isChecking = useSelector(({ auth }) => auth.isChecking);
+
   useEffect(() => {
-    store.dispatch(listenToAuthChanges());
+    dispatch(listenToAuthChanges());
   }, []);
 
+  return isChecking ? (
+    <LoadingView />
+  ) : (
+    <Router>
+      <ContentWrapper>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Welcome />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/chat/:id">
+            <Chat />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Switch>
+      </ContentWrapper>
+    </Router>
+  );
+};
+
+const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="content-wrapper">
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Welcome />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/chat/:id">
-              <Chat />
-            </Route>
-            <Route path="/settings">
-              <Settings />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </Provider>
+    <StoreProvider>
+      <ChatApp />
+    </StoreProvider>
   );
 };
 
